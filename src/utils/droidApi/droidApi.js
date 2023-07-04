f6c897a31ed7d2473bde813007243c370df0680c
nikameru <nika.ra.txt@gmail.com>
2023-07-04 23:57:15 +0300

    added basic dan system and response formatter

--- src/utils/droidApi/droidApi.js
+++ src/utils/droidApi/droidApi.js
@@ -21,12 +21,36 @@ async function getRecentPlays(uid, amount) {
             }
         });
 
+        responseFormatter(plays);
+
         return plays;
     } else {
         return false;
     }
 }
 
-//getRecentPlays(68403, 2);
+function responseFormatter(plays) {
+    var play;
+    var formattedPlay;
+
+    for (let i = 0; i < plays.length; i++) {
+        play = plays.shift();
+        formattedPlay = {};
+
+        formattedPlay.title = play.substring(0, play.indexOf('\n\n\n')).trim();
+        formattedPlay.date = play.substring(play.indexOf('\n\n\n'), play.indexOf(' / score: ')).trim();
+        formattedPlay.score = play.substring(play.indexOf(' / score: ') + 10, play.indexOf(' / mod: ')).replaceAll(',', '');
+        formattedPlay.mods = play.substring(play.indexOf(' / mod: ') + 8, play.indexOf(' / combo: '));
+        formattedPlay.combo = play.substring(play.indexOf(' / combo: ') + 10, play.indexOf(' x / accuracy: '));
+        formattedPlay.accuracy = play.substring(play.indexOf(' x / accuracy: ') + 15, play.indexOf('%\n miss: '));
+        formattedPlay.misscount = play.substring(play.indexOf('%\n miss: ') + 9, play.indexOf('\n{""hash":'));
+    
+        plays.push(formattedPlay);
+    }
+
+    console.log(plays);
+}
+
+//getRecentPlays(68403, 3);
 
 module.exports = { getRecentPlays };
