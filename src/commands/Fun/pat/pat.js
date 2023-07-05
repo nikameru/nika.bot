b261ff790eb770745c93dc1be33644dd825819b3
nikameru <nika.ra.txt@gmail.com>
2023-07-05 18:45:25 +0300

    unable to use canvas, temporarily removed /pat

--- src/commands/Fun/pat/pat.js
+++ src/commands/Fun/pat/pat.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageAttachment } = require('discord.js');
-const petpetgif = require('pet-pet-gif');
+//const petpetgif = require('pet-pet-gif');
 
 module.exports = {
     async run(client, interaction) {
@@ -11,17 +11,17 @@ module.exports = {
         if (patMember == interaction.member) patDescription = `**${interaction.member.displayName} pats themselves!**`;
         else patDescription = `**${interaction.member.displayName} pats ${patMember}!**`;
 
-        const gif = await petpetgif(patAvatar);
-        const attachmentGif = new MessageAttachment(gif, 'pat.gif');
+        //const gif = await petpetgif(patAvatar);
+        //const attachmentGif = new MessageAttachment(gif, 'pat.gif');
 
         const patEmbed = new MessageEmbed()
             .setDescription(patDescription)
             .setColor('#fcbef9')
-            .setImage('attachment://pat.gif')
+            //.setImage('attachment://pat.gif')
             .setFooter({ text: 'nika.bot', iconURL: client.user.displayAvatarURL() })
             .setTimestamp();
 
-        interaction.reply({ embeds: [patEmbed], files: [attachmentGif] });
+        //interaction.reply({ embeds: [patEmbed], files: [attachmentGif] });
     },
     config: new SlashCommandBuilder()
         .setName('pat')
