625ff405353f4e67672c2d25f7ef5c06bb542b4d
nikameru <nika.ra.txt@gmail.com>
2023-07-05 23:21:44 +0300

    minor rename

--- src/commands/Osu!droid/course/course.js
+++ src/commands/Osu!droid/course/course.js
@@ -72,8 +72,8 @@ const config = new SlashCommandBuilder()
                     .setDescription('Name of the Course.')
                     .setRequired(true)
                     .addChoices(
-                        { name: 'Aim Death Skill Course', value: 'aim' },
-                        { name: 'Aim Death Skill Course+', value: 'aimplus' },
+                        { name: 'Aim Death Skill Course', value: 'daim' },
+                        { name: 'Aim Death Skill Course+', value: 'daimplus' },
                         { name: 'Aim Easy Skill Course', value: 'eaim' },
                         { name: 'Aim Easy Skill Course+', value: 'eaimplus' }
                         
